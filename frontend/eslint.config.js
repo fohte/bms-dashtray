6c067e6fbbcd20ea59c4b09777ba9e5a3dafeb40
Hayato Kawai <fohte.hk@gmail.com>
2026-06-11 22:23:30 +0900

    Revert broken renovate copier commit

---
+++ frontend/eslint.config.js
@@ -0,0 +1,39 @@
+import { config } from '@fohte/eslint-config'
+import storybook from 'eslint-plugin-storybook'
+
+export default config(
+  { typescript: { typeChecked: true } },
+  ...storybook.configs['flat/recommended'],
+  {
+    files: ['**/*.ts{,x}'],
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['.storybook/*.ts', '.storybook/*.tsx'],
+        },
+      },
+    },
+  },
+  {
+    rules: {
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: ['./*', '../*'],
+              message:
+                'Please use absolute imports instead of relative imports.',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ['.storybook/**', 'vitest.config.ts'],
+    rules: {
+      'no-restricted-imports': 'off',
+    },
+  },
+)
